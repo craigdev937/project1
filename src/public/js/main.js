39a3cc01d12cb130bba7f50fb5425a1881a6446d
Craig <indigomx9@gmail.com>
2021-10-18 15:10:18 -0400

    Updated the Header Section.

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,5 +1,29 @@
-console.log("Handlebars!");
+let menuBtn = document.querySelector("#menu-btn");
+let searchBtn = document.querySelector("#search-btn");
+let colorBtn = document.querySelector("#color-btn");
+let themeBtn = document.querySelector("#theme-btn");
 
+let navbar = document.querySelector(".navbar");
+let searchForm = document.querySelector(".search-form");
+let colorsPalette = document.querySelector(".colors-palette");
+
+menuBtn.onclick = () => {
+    navbar.classList.toggle("active");
+    searchForm.classList.remove("active");
+    colorsPalette.classList.remove("active");
+};
+
+searchBtn.onclick = () => {
+    searchForm.classList.toggle("active");
+    navbar.classList.remove("active");
+    colorsPalette.classList.remove("active");
+};
+
+colorBtn.onclick = () => {
+    colorsPalette.classList.toggle("active");
+    navbar.classList.remove("active");
+    searchForm.classList.remove("active");
+};
 
 
 
