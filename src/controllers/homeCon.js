cac60b7c446b50ef0b3d972d9cedd8b40faffb03
Craig <indigomx9@gmail.com>
2022-03-28 19:35:59 -0400

    Adding other CSS pages.

--- src/controllers/homeCon.js
+++ src/controllers/homeCon.js
@@ -14,6 +14,30 @@ class Pages {
             javaSCRIPT: "math.js"
         })
     };
+
+    Display = (req, res) => {
+        res.render("pages/display", {
+            pageTitle: "Display",
+            styleCSS: "display.css",
+            javaSCRIPT: "display.js"
+        })
+    };
+
+    Position = (req, res) => {
+        res.render("pages/position", {
+            pageTitle: "Position",
+            styleCSS: "position.css",
+            javaSCRIPT: "position.js"
+        })
+    };
+
+    Flexbox = (req, res) => {
+        res.render("css/flex", {
+            pageTitle: "Flexbox",
+            styleCSS: "flex.css",
+            javaSCRIPT: "flex.js"
+        })
+    };
 };
 
 export const PAGES = new Pages;
--- src/routes/homeRt.js
+++ src/routes/homeRt.js
@@ -4,6 +4,9 @@ import { PAGES } from "../controllers/homeCon.js";
 export const homeRt = express.Router();
     homeRt.get("/", PAGES.HomeIndex);
     homeRt.get("/math", PAGES.Math);
+    homeRt.get("/display", PAGES.Display);
+    homeRt.get("/position", PAGES.Position);
+    homeRt.get("/flex", PAGES.Flexbox);
 
 
 
